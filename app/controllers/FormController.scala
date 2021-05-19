b86d72caac6f3b786de166fdc8404f6d86f2e94d
Daniel Carter <carterdaniel@hotmail.com>
2021-05-19 11:53:27 +0100

    added form which connects to back end to pass data

---
+++ app/controllers/FormController.scala
@@ -0,0 +1,67 @@
+package controllers
+
+import models.Vehicle
+import play.api.libs.json.Json
+import play.api.data.Form
+import play.api.data.Forms.{mapping, text}
+import play.api.libs.ws
+import play.api.libs.ws.{WSClient, WSResponse}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, Request}
+
+import javax.inject._
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class FormController @Inject()(ws: WSClient, cc: ControllerComponents, implicit val ec: ExecutionContext) extends AbstractController(cc) with play.api.i18n.I18nSupport {
+  def simpleForm() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.form(BasicForm.form))
+  }
+
+  def simpleFormPost(): Action[AnyContent] = Action.async { implicit request =>
+    val postData = request.body.asFormUrlEncoded
+
+    val vehicleName = postData.map { args =>
+      args("Vehicle Name").head
+    }.getOrElse(Ok("Error"))
+
+    val dataToBeSent = Json.obj(
+      "Vehicle Name" -> s"${vehicleName}"
+    )
+    val futureResponse: Future[WSResponse] = ws.url("http://localhost:9001/form").post(dataToBeSent)
+
+    futureResponse.map {
+      response =>
+        val js = Json.fromJson[Vehicle](response.json)
+        val veh = js.get
+        Ok(views.html.vehicle(veh))
+    } recover {
+      case _ => NotFound
+    }
+  }
+}
+  case class BasicForm(name: String)
+
+  // this could be defined somewhere else,
+  // but I prefer to keep it in the companion object
+  object BasicForm {
+    val form: Form[BasicForm] = Form(
+      mapping(
+        //      "wheels"    -> number,
+        //      "heavy"     -> boolean,
+        "name" -> text
+      )(BasicForm.apply)(BasicForm.unapply)
+    )
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,25 +11,18 @@ import repositories.DataRepository
 
 import scala.concurrent.{ExecutionContext, Future}
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+
 @Singleton
-class HomeController @Inject()(ws:WSClient, val controllerComponents: ControllerComponents, dataRepository: DataRepository, implicit val ec: ExecutionContext) extends BaseController {
+class HomeController @Inject()(ws:WSClient,
+                               val controllerComponents: ControllerComponents,
+                               implicit val ec: ExecutionContext) extends BaseController {
 
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
 
   }
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+
   def vehicle(vehicleName: String) = Action.async { implicit request: Request[AnyContent] =>
     val futureResult = ws.url(s"http://localhost:9001/checkVehicle/${vehicleName}").get()
     futureResult.map { response =>
